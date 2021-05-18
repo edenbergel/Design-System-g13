0c756a95146fe845721d7449ac950288cb4cce1e
Eden BERGEL <eb@wshop-cloudcommerce.com>
2021-05-18 14:21:56 +0200

    inte tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -5,12 +5,12 @@ function Tabs(props) {
   
   return (
     <div className='tabs__wrapper'>
-      <h3 className={props.signup === true ? 'active' : ''} onClick={props.formSignUp}>
+      <h2 className={props.signup === true ? 'active' : ''} onClick={props.formSignUp}>
         Se connecter
-      </h3>
-      <h3 className={props.signup === false ? 'active' : ''} onClick={props.formSignIn}>
+      </h2>
+      <h2 className={props.signup === false ? 'active' : ''} onClick={props.formSignIn}>
         S'inscrire
-      </h3>
+      </h2>
     </div>
   )
 }
